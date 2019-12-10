d93f6228a72071935a0fdedcbe521bd1aeb75150
Phuong Nguyen <phuongnn2@ocb.com.vn>
2019-12-10 23:11:25 +0700

    cuoi ky lan 2

---
+++ src/scenes/TeamInformation/Notification/index.js
@@ -0,0 +1,204 @@
+import React, { Component } from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    ImageBackground,
+    Image,
+    FlatList
+} from 'react-native'
+import axios from 'axios'
+import { baseURL } from '../../../configs';
+
+
+class FlatFlistItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            firstNameSender: '',
+            lastNameSender: ''
+        }
+    }
+    componentDidMount() {
+        const { item } = this.props;
+        this.getPersonalInfo(item.id_sender)
+    }
+
+    getPersonalInfo = (id_login) => {
+        const data = {
+            id_login
+        }
+        // console.log(id_login)
+        const config = {
+            'Content-Type': 'application/json',
+        };
+        axios.post(baseURL + '/player/getPersonalInfo', data, config)
+            .then((response) => {
+                console.log(response)
+                this.setState({
+                    firstNameSender: response.data.rows[0].first_name,
+                    lastNameSender: response.data.rows[0].last_name,
+                })
+
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
+
+    _addToTeam = () => {
+        const answer = 1; const type = 1;
+        const { item, team_id } = this.props;
+        const data = {
+            id_record: item.id_record,
+            id_sender: item.id_sender,
+            id_team: team_id.id,
+            type,
+            answer
+        }
+        const config = {
+            'Content-Type': 'application/json',
+        };
+        axios.post(baseURL + '/team/addToTeam', data, config)
+            .then(response => {
+                console.log(response);
+                if (response.status === 200) {
+                    this.props.goToTeamInformation();
+                }
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+    render() {
+        const { item, team_id } = this.props;
+        console.log(team_id);
+        const { firstNameSender, lastNameSender } = this.state;
+        // console.log(personalInfo)
+        console.log(item);
+        return (
+            <View style={styles.container}>
+                <View style={{ flexDirection: 'row', flex: 5, }}>
+                    <Text>{firstNameSender + lastNameSender}</Text>
+                    <Text> xin gia nhập team</Text>
+                </View>
+                <View style={styles.leftPanel}>
+                    <View style={styles.containerButton}>
+                        <TouchableOpacity style={styles.button}
+                            onPress={this._addToTeam}
+                        >
+                            <Text>Đồng ý</Text>
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.containerButton}>
+                        <TouchableOpacity style={[styles.button, { backgroundColor: 'red' }]}>
+                            <Text>Từ chối</Text>
+                        </TouchableOpacity>
+                    </View>
+                </View >
+            </View >
+        )
+    }
+
+
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginLeft: 15,
+        marginRight: 15,
+        height: 50,
+        borderBottomWidth: 1,
+    },
+    leftPanel: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        flex: 2,
+    },
+    containerButton: {
+        alignItems: 'center',
+        width: '50%'
+        // flex: 2,
+    },
+    button: {
+        width: '100%',
+        height: '80%',
+        backgroundColor: '#4CAF50',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 10,
+    },
+})
+
+export default class Notification extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            listNotification: [],
+        }
+    }
+    componentWillMount() {
+        this.getNotification();
+    }
+    componentDidMount() {
+
+
+    }
+
+    _goToTeamInformation = () => {
+        this.props.navigation.navigate('TeamInformation')
+    }
+    getNotification = () => {
+        const { navigation } = this.props;
+        const params = navigation.getParam('params')
+        const id_login = navigation.getParam('id_login');
+        const data = {
+            id_receiver: id_login
+        };
+        const config = {
+            'Content-Type': 'application/json',
+        };
+        axios.post(baseURL + '/team/getNotification', data, config)
+            .then(response => {
+                console.log(response);
+                this.setState({
+                    listNotification: response.data.rows,
+                    team_id: params,
+                })
+            })
+            .catch(error => {
+                console.log(error)
+            })
+    }
+    renderNotification = () => {
+        console.log('render')
+        const { listNotification, team_id } = this.state;
+        return (
+            <FlatList
+                data={listNotification}
+                keyExtractor={item => (item.id_record).toString()}
+                renderItem={({ item }) =>
+                    <FlatFlistItem
+                        item={item}
+                        team_id={team_id}
+                        goToTeamInformation={this._goToTeamInformation} />}
+            ></FlatList>
+        )
+
+    }
+    render() {
+        const { listNotification } = this.state;
+        // const data = listNotification
+        // console.log(listNotification.map())
+        return (
+            <View>
+                {this.renderNotification()}
+            </View>
+        )
+    }
+}
---
+++ src/scenes/TeamInformation/Training/index.js
@@ -0,0 +1,84 @@
+import React, { Component } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import CalendarPicker from 'react-native-calendar-picker';
+import axios from 'axios';
+import { baseURL } from '../../../configs';
+
+export default class Training extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedStartDate: null,
+            pickedDate: false
+        };
+        this.onDateChange = this.onDateChange.bind(this);
+    }
+
+    onDateChange(date) {
+        this.setState({
+            selectedStartDate: date,
+            pickedDate: true
+        });
+    }
+
+    _setDate = () => {
+        const { navigation } = this.props;
+        const params = navigation.getParam('params');
+        console.log('teamInfo: ', params);
+        const { selectedStartDate } = this.state;
+        const date = selectedStartDate.toString();
+        const data = {
+            date_training: date,
+            team_id: params.id,
+            type: 2,
+        }
+        const config = {
+            'Content-Type': 'application/json',
+        }
+        // console.log(date)
+        axios.post(baseURL + '/team/training', data, config)
+            .then(response => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error);
+            })
+
+    }
+    render() {
+        const { selectedStartDate } = this.state;
+        // const startDate = selectedStartDate ? selectedStartDate.toString() : '';
+        return (
+            <View style={styles.container}>
+                <CalendarPicker
+                    onDateChange={this.onDateChange}
+                    selectedDayColor='green'
+                />
+
+                <View style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'center', marginBottom: 20, }}>
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={this._setDate}>
+                        <Text>Đặt lịch tập luyện</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
+        );
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#FFFFFF',
+        marginTop: 100,
+    },
+    button: {
+        width: '90%',
+        alignSelf: 'center',
+        backgroundColor: '#4CAF50',
+        height: 50,
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
